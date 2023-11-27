0eda1883d8ce0d8717495c95d7824ea0b11de17e
tflanigan <tflanig1@gmail.com>
2023-11-26 21:23:40 -0600

    5 start tests (#6)
    * starting to add tests
    
    * Working test for echo
    
    * review edits

--- bot/fambot.py
+++ bot/fambot.py
@@ -6,44 +6,38 @@ class FamBot(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.logger = logging.getLogger(__name__)
-        self.call_response = {
-            'help': self._help,
-            'echo': self._echo,
-            'introduce': self._introduce
-        }
 
     async def on_ready(self):
         self.logger.info(f'Connected to Discord as {self.user}')
 
     async def on_message(self, message):
-        #TODO: More precise check by looking for member of bot group.
-        if message.author == self.user:
-            return
+        # #TODO: More precise check by looking for member of bot group.
+        # if message.author == self.user:
+        #     return
 
         if message.content.startswith('$fambot'):
-            #TODO: Use argparse to parse arguments.
+            #TODO: Use argparse or something better to parse arguments.
             args = message.content.split()[1:]
             call = args[0].strip().lower()
-            if call == 'shutdown':
-                await message.channel.send('Goodbye, I am going to take a nap now.')
-                await self.close()
-            else:
-                method = self.call_response.get(call, self._help)
-                await method(message, args)
+            if call == 'help':
+                await self.help(message, args)
+            elif call == 'echo':
+                await self.echo(message, args)
 
-    def _help(self, message, args):
-        return message.channel.send('Help menu coming soon...')
+    async def help(self, message, args):
+        await message.channel.send('$fambot echo <message>: repeat back a message')
 
-    def _echo(self, message, args):
-        return message.channel.send(' '.join(args[1:]))
-    
-    def _introduce(self, message, args):
-        return message.channel.send('Hello, I am FamBot. I am still a baby and take lots of naps but I will learn more soon.')
+    async def echo(self, message, args):
+        await message.channel.send(' '.join(args[1:]))
 
-intents = discord.Intents.none()
-intents.messages = True
-intents.message_content = True
+def get_client():
+    intents = discord.Intents.none()
+    intents.guilds = True
+    intents.messages = True
+    intents.message_content = True
 
-client = FamBot(intents=intents)
+    return FamBot(intents=intents)
 
-client.run(os.environ.get("DISCORD_TOKEN"), log_level=logging.DEBUG)
+if __name__ == '__main__':
+    client = get_client()
+    client.run(os.environ.get("DISCORD_TOKEN"), log_level=logging.DEBUG)
---
+++ tests/test_integration.py
@@ -0,0 +1,34 @@
+import asyncio
+import os
+import pytest
+import pytest_asyncio
+import uuid
+import bot.fambot as fambot
+
+class TestIntegrationDiscord:
+    @pytest_asyncio.fixture
+    async def client(self):
+        client = fambot.get_client()
+        yield client
+        await client.close()
+
+    @pytest_asyncio.fixture
+    async def connected_client(self, client):
+        await client.login(os.environ.get("DISCORD_TOKEN"))
+        client_task = asyncio.create_task(
+            client.connect())
+        await client.wait_until_ready()
+        yield client
+        await client.close()
+        await client_task
+
+    @pytest.mark.asyncio
+    async def test_echo(self, connected_client):
+        """
+        Test that the bot can echo a unique message and read back the value.
+        """
+        bot_channel = connected_client.get_channel(int(os.environ.get("CHANNEL_ID")))
+        test_message = str(uuid.uuid4())
+        await bot_channel.send(f'$fambot echo {test_message}')
+        await asyncio.sleep(1)
+        assert bot_channel.last_message.content == test_message
