9f52b8653be41777d26496595091b636e9d2424d
Thomas Flanigan <tflanig1@gmail.com>
2023-11-27 02:09:38 -0600

    Weather basic text functionality working

--- bot/fambot.py
+++ bot/fambot.py
@@ -1,3 +1,5 @@
+import aiohttp
+import asyncio
 import discord
 import logging
 import os
@@ -23,12 +25,35 @@ class FamBot(discord.Client):
                 await self.help(message, args)
             elif call == 'echo':
                 await self.echo(message, args)
+            elif call == 'compare-weather':
+                await self.compare_weather(message, args)
 
     async def help(self, message, args):
         await message.channel.send('$fambot echo <message>: repeat back a message')
 
     async def echo(self, message, args):
         await message.channel.send(' '.join(args[1:]))
+
+    async def compare_weather(self, message, args):
+        base_url = f"https://api.weatherapi.com/v1/current.json?key={os.environ.get('WEATHER_API_KEY')}"
+        args = "&q={}&aqi=no"
+
+        async with aiohttp.ClientSession() as session:
+            tasks = [self._call_api(base_url + args.format(loc), session=session) for loc in os.environ.get("WEATHER_LOCATIONS").split(",")]
+            responses = await asyncio.gather(*tasks)
+            result = 'The current temperature and weather conditions are...\n'
+            for resp in responses:
+                result += f'{resp["location"]["name"]}, {resp["location"]["region"]}: {resp["current"]["temp_f"]}F and {resp["current"]["condition"]["text"]}\n'
+            await message.channel.send(result)
+
+    async def _call_api(self, url, session=None):
+        if session is None:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as resp:
+                    return await resp.json()
+        else:
+            async with session.get(url) as resp:
+                return await resp.json()
 
 def get_client():
     intents = discord.Intents.none()
