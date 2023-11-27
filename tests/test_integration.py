1c926f7914aae2e1962416bb04d862fcab5f633d
Thomas Flanigan <tflanig1@gmail.com>
2023-11-26 20:16:08 -0600

    Working test for echo

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,26 +1,34 @@
-import logging
+import asyncio
 import os
 import pytest
+import pytest_asyncio
 import uuid
 import bot.fambot as fambot
 
 class TestIntegration:
-    # def test_self_message_ignored(self):
-    #     """
-    #     Test that the bot ignores messages from itself.
-    #     """
-    #     assert False
+    @pytest_asyncio.fixture
+    async def client(self):
+        client = fambot.get_client()
+        yield client
+        await client.close()
 
-    def test_echo(self):
+    @pytest_asyncio.fixture
+    async def connected_client(self, client):
+        await client.login(os.environ.get("DISCORD_TOKEN"))
+        client_task = asyncio.create_task(
+            client.connect())
+        await client.wait_until_ready()
+        yield client
+        await client.close()
+        await client_task
+
+    @pytest.mark.asyncio
+    async def test_echo(self, connected_client):
         """
         Test that the bot can echo a unique message and read back the value.
         """
-        client = fambot.get_client()
-        client.run(os.environ.get("DISCORD_TOKEN"), log_level=logging.DEBUG)
-        print('breakpoint-1')
-
-    def test_help(self):
-        """
-        Test that the bot can respond to a help command.
-        """
-        assert False
+        bot_channel = connected_client.get_channel(int(os.environ.get("CHANNEL_ID")))
+        test_message = str(uuid.uuid4())
+        await bot_channel.send(f'$fambot echo {test_message}')
+        await asyncio.sleep(1)
+        assert bot_channel.last_message.content == test_message
